e6de7d01b6b500025250048b346e5ffc919c924e
CipherBoi007 <cipherboi007@gmail.com>
2025-04-26 22:43:50 +0530

    Refactor routing structure in main.jsx to include all application routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './index.css';
+import App from './App.jsx';
+import CareerAssessmentPage from './pages/CareerAssessmentPage';
+import CareerGuidancePage from './pages/CareerGuidancePage';
+import LoginPage from './pages/LoginPage';
+import PersonalDetailsPage from './pages/PersonalDetailsPage';
+import RegisterPage from './pages/RegisterPage';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/career-assessment" element={<CareerAssessmentPage />} />
+        <Route path="/career-guidance" element={<CareerGuidancePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/personal-details" element={<PersonalDetailsPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
-)
+);
